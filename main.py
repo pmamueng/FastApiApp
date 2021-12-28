cd433cbc1a75272d53698afb823fcc95145c1e65
pmamueng <ping.mamueng@illuminate.ai>
2021-12-28 17:29:41 -0600

    Add extra endpoint, add POST request

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to my API"}
+
+
+@app.get("/posts")
+def get_post():
+    return {"data": "This is your post"}
+
+
+@app.post("/create_posts")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": {'title': payload['title'], 'content': payload['content']}}
