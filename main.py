4b3ae692543122ef4fc089ccfa0d4f8e6952ef12
pmamueng <ping.mamueng@illuminate.ai>
2021-12-29 13:41:43 -0600

    Add schema with required field, optional field with default value, and optional field | Add pydantic to requirements

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
+
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # Optional field, if user doesn't provide value then it will default to True
+    rating: Optional[int] = None  # Complete optional, if user doesn't provide value then nothing will be assigned
 
 
 @app.get("/")
@@ -15,6 +24,8 @@ def get_post():
 
 
 @app.post("/create_posts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": {'title': payload['title'], 'content': payload['content']}}
+def create_post(new_post: Post):
+    print(new_post)
+    print(new_post.dict())
+    return {"data": new_post}
+# title str, content str
