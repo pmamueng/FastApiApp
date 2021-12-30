73c126243a2ceb72c9576d8b14a6b286c75bf91a
pmamueng <pmamueng.dev@gmail.com>
2021-12-29 21:43:12 -0600

    12292021: Implement schema for POST and update GET methods (#3)

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
-from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+from typing import Optional
+
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True  # Optional field, if user doesn't provide value then it will default to True
+    rating: Optional[int] = None  # Complete optional, if user doesn't provide value then nothing will be assigned
+
+
+app_posts = [{"title": "example_title_1", "content": "example_content_1", "id": 1},
+             {"title": "example_title_2", "content": "example_content_2", "id": 2}]
+
+
+def find_post(id):
+    for p in app_posts:
+        if p["id"] == id:
+            return p
 
 
 @app.get("/")
@@ -10,11 +30,20 @@ def root():
 
 
 @app.get("/posts")
-def get_post():
-    return {"data": "This is your post"}
+def get_posts():
+    return {"data": app_posts}
 
 
-@app.post("/create_posts")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": {'title': payload['title'], 'content': payload['content']}}
+@app.get("/posts/{id}")
+def get_post(id: int):  # turn id into string and FastAPI will validate it
+    post = find_post(id)  # id is a string from path parameter, have to convert it to integer
+    return {"data": post}
+
+
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    app_posts.append(post_dict)
+    return {"data": post_dict}
+
