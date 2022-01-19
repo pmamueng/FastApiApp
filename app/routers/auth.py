c9ba065674a9956b2076660b9ea9307361d6a68e
pmamueng <pmamueng.dev@gmail.com>
2022-01-18 21:00:41 -0600

    01182022: Return user after verify_access_token when using posts router (#19)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -21,11 +21,11 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
     if not user:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
-    
+
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Invalid Credentials")
 
-    access_token = oauth2.create_access_token(data = {"user_id": user.id})
+    access_token=oauth2.create_access_token(data={"user_id": user.id})
 
-    return {"access_token" : access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/routers/post.py
+++ app/routers/post.py
@@ -14,15 +14,20 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.ResponsePost])
-def get_posts(db: Session = Depends(get_db), skip: int = 0, limit: int = 100, 
-              user_id: int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), 
+              skip: int = 0, 
+              limit: int = 100, 
+              current_user: int = Depends(oauth2.get_current_user)):
+
     posts = db.query(models.Post).offset(skip).limit(limit).all()
     return posts
 
 
 @router.get("/{id}", response_model=schemas.ResponsePost)
-def get_post_by_id(id: int, db: Session = Depends(get_db), 
-                   user_id: int = Depends(oauth2.get_current_user)):
+def get_post_by_id(id: int, 
+                   db: Session = Depends(get_db), 
+                   current_user: int = Depends(oauth2.get_current_user)):
+
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -31,9 +36,11 @@ def get_post_by_id(id: int, db: Session = Depends(get_db),
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ResponsePost)
-def create_post(post: schemas.CreatePost, db: Session = Depends(get_db), 
-                user_id: int = Depends(oauth2.get_current_user)):
-    print(user_id)
+def create_post(post: schemas.CreatePost, 
+                db: Session = Depends(get_db), 
+                current_user: int = Depends(oauth2.get_current_user)):
+
+    print(current_user.email)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -42,8 +49,10 @@ def create_post(post: schemas.CreatePost, db: Session = Depends(get_db),
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db), 
-                user_id: int = Depends(oauth2.get_current_user)):
+def delete_post(id: int, 
+                db: Session = Depends(get_db), 
+                current_user: int = Depends(oauth2.get_current_user)):
+
     deleted_post = db.query(models.Post).filter(models.Post.id == id).first()
     if not deleted_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -54,8 +63,11 @@ def delete_post(id: int, db: Session = Depends(get_db),
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.ResponsePost)
-def update_post(id: int, updated_post: schemas.PostBase, db: Session = Depends(get_db), 
-                user_id: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, 
+                updated_post: schemas.PostBase, 
+                db: Session = Depends(get_db), 
+                current_user: int = Depends(oauth2.get_current_user)):
+
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
     if post == None:
