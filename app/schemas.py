a95b3b0b39e571035af73a57c3b8a4b2c3569bb5
pmamueng <pmamueng.dev@gmail.com>
2022-01-23 20:21:33 -0600

    01232022: Implement like system with vote model and schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, conint, EmailStr
 from typing import Optional
 
 
@@ -49,3 +49,8 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[str] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
