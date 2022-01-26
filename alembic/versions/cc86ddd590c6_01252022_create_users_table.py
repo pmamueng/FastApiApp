2b9b64fcba5c1e7213339bc6de841636fa29f05a
pmamueng <pmamueng.dev@gmail.com>
2022-01-25 20:16:07 -0600

    01252022: Integrate Alembic as database migration tool (#26)
    Utilize Alembic to create posts and users table

---
+++ alembic/versions/cc86ddd590c6_01252022_create_users_table.py
@@ -0,0 +1,32 @@
+"""01252022: Create users table
+
+Revision ID: cc86ddd590c6
+Revises: 44e10cb24249
+Create Date: 2022-01-25 20:09:04.281097
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cc86ddd590c6'
+down_revision = '44e10cb24249'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('users', 
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('password', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), server_default=sa.text('now()'), nullable=False),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('email'))
+    pass
+
+
+def downgrade():
+    op.drop_table('users')
+    pass
