c9e87d92368785b93995937db678708d2cc1c4c9
pmamueng <pmamueng.dev@gmail.com>
2022-02-24 22:41:14 -0600

    02162022: Add database creation before test module runs and tear down after | tests still depend on each other

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,20 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
 from app import schemas
+from tests.database import client, session
 
-client = TestClient(app)
 
-
-def test_root():
+def test_root(client):
     res = client.get("/")
     assert res.json().get('message') == 'Welcome to my API'
     assert res.status_code == 200
 
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json = {'email':'test_email@test.com','password':'test_password'})
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == 'test_email@test.com'
     assert res.status_code == 201
+
+
+def test_login_user(client):
+    res = client.post("/login", data = {'username':'test_email@test.com','password':'test_password'})
+    assert res.status_code == 200
