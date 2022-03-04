672247966e913a5cdc18dc2deda9d310be8b80ce
pmamueng <pmamueng.dev@gmail.com>
2022-03-03 20:09:57 -0600

    03032022: Add get posts (one/all) pytest module (#35)

---
+++ tests/test_posts.py
@@ -0,0 +1,37 @@
+import pytest
+
+from app import schemas
+from tests.conftest import authorized_client
+
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+
+    def validate(post):
+        return schemas.ResponsePostVotesCount(**post)
+    map(validate, res.json())
+    assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/88888")
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.ResponsePostVotesCount(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.title == test_posts[0].title
+    assert post.Post.content == test_posts[0].content
