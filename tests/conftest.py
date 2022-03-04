956aa497d0c63719786dc6e4bbc711e483f6e1ed
pmamueng <pmamueng.dev@gmail.com>
2022-03-03 20:09:24 -0600

    03032022: Add get posts (one/all) pytest module

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,11 @@ from sqlalchemy import create_engine, over
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from app import models
 from app.config import settings
 from app.database import Base, get_db
 from app.main import app
+from app.oauth2 import create_access_token
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test"
@@ -49,3 +51,48 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
+
+
+@pytest.fixture()
+def token(test_user):
+    return create_access_token({"user_id": test_user['id']})
+    
+
+@pytest.fixture()
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+
+@pytest.fixture()
+def test_posts(test_user, session):
+    posts_data = [
+        {
+            'title': 'first_test_title',
+            'content': 'first_test_content',
+            'user_id': test_user['id']
+        },
+        {
+            'title': 'second_test_title',
+            'content': 'second_test_content',
+            'user_id': test_user['id']
+        },
+        {
+            'title': 'third_test_title',
+            'content': 'third_test_content',
+            'user_id': test_user['id']
+        }
+    ]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+    session.add_all(posts)
+    session.commit()
+    posts = session.query(models.Post).all()
+    return posts
