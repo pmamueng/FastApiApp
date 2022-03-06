56b6c63ae432d813ba0a1b3eb4daaeb60644628b
pmamueng <pmamueng.dev@gmail.com>
2022-03-05 20:39:35 -0600

    03052022: Add tests for update posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -85,3 +85,41 @@ def test_delete_post_non_exist(authorized_client, test_user, test_posts):
 def test_delete_other_user_post(authorized_client, test_user, test_posts):
     res = authorized_client.delete(f"/posts/{test_posts[3].id}")
     assert res.status_code == 403
+
+
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated_title",
+        "content": "updated_content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json = data)
+    updated_post = schemas.ResponsePost(**res.json())
+    assert res.status_code == 202
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
+
+
+def test_update_other_user_post(authorized_client, test_user, test_user_2, test_posts):
+    data = {
+        "title": "updated_title",
+        "content": "updated_content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json = data)
+    assert res.status_code == 403
+
+
+def test_unauthorized_user_update_post(client, test_user, test_posts):
+    res = client.put(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_update_post_non_exist(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated_title",
+        "content": "updated_content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/88888", json = data)
+    assert res.status_code == 404
